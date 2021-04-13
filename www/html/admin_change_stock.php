36e2e82c9a5f331adeae87ecae983514ad02565d
tamatsukuri01 <ysk0102tama@gmail.com>
2021-04-13 18:38:02 +0900

    CSRF対策

--- www/html/admin_change_stock.php
+++ www/html/admin_change_stock.php
@@ -9,24 +9,30 @@ require_once MODEL_PATH . 'user.php';
 require_once MODEL_PATH . 'item.php';
 //セッションスタート
 session_start();
-
+//ログインチェック関数
 if (is_logined() === false) {
+  //ログインしていない場合ログインページへリダイレクト
   redirect_to(LOGIN_URL);
 }
-
+//db接続関数
 $db = get_db_connect();
 
+//ログインユーザー情報取得
 $user = get_login_user($db);
 
+//管理者チェック
 if (is_admin($user) === false) {
   redirect_to(LOGIN_URL);
 }
 
+//POSTデータ取得し変数へ
 $item_id = get_post('item_id');
 $stock = get_post('stock');
 $token = get_post('token');
 
+//トークンのチェック関数
 if (is_valid_csrf_token($token)) {
+  //在庫数の変更処理
   if (update_item_stock($db, $item_id, $stock)) {
     set_message('在庫数を変更しました。');
   } else {
