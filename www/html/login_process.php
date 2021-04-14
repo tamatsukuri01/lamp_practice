14c27e1424fd47279eec96487a7a65ee09212505
tamatsukuri01 <ysk0102tama@gmail.com>
2021-04-14 10:14:57 +0900

    CSRF対策

--- www/html/login_process.php
+++ www/html/login_process.php
@@ -5,7 +5,7 @@ require_once MODEL_PATH . 'user.php';
 
 session_start();
 
-if(is_logined() === true){
+if (is_logined() === true) {
   redirect_to(HOME_URL);
 }
 
@@ -18,16 +18,17 @@ $db = get_db_connect();
 
 $user = login_as($db, $name, $password);
 
-if(is_valid_csrf_token($token)) {
-if( $user === false){
+
+if ($user === false) {
   set_error('ログインに失敗しました。');
   redirect_to(LOGIN_URL);
 }
 
-set_message('ログインしました。');
-if ($user['type'] === USER_TYPE_ADMIN){
-  redirect_to(ADMIN_URL);
-}
+if (is_valid_csrf_token($token)) {
+  set_message('ログインしました。');
+  if ($user['type'] === USER_TYPE_ADMIN) {
+    redirect_to(ADMIN_URL);
+  }
 } else {
   set_error('不正な操作が行われました。');
   redirect_to(LOGIN_URL);
--- www/html/signup_process.php
+++ www/html/signup_process.php
@@ -5,27 +5,33 @@ require_once MODEL_PATH . 'user.php';
 
 session_start();
 
-if(is_logined() === true){
+if (is_logined() === true) {
   redirect_to(HOME_URL);
 }
 
 $name = get_post('name');
 $password = get_post('password');
 $password_confirmation = get_post('password_confirmation');
+$token = get_post('token');
 
 $db = get_db_connect();
 
-try{
+try {
   $result = regist_user($db, $name, $password, $password_confirmation);
-  if( $result=== false){
+  if ($result === false) {
     set_error('ユーザー登録に失敗しました。');
     redirect_to(SIGNUP_URL);
   }
-}catch(PDOException $e){
+} catch (PDOException $e) {
   set_error('ユーザー登録に失敗しました。');
   redirect_to(SIGNUP_URL);
 }
 
-set_message('ユーザー登録が完了しました。');
-login_as($db, $name, $password);
-redirect_to(HOME_URL);
+if (is_valid_csrf_token($token)) {
+  set_message('ユーザー登録が完了しました。');
+  login_as($db, $name, $password);
+  redirect_to(HOME_URL);
+} else {
+  set_error('不正な操作が行われました。');
+  redirect_to(SIGNUP_URL);
+}
