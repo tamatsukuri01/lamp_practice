c6e6525571d17cab90b7bb2ef3527f81659466ac
tamatsukuri01 <ysk0102tama@gmail.com>
2021-04-14 22:33:09 +0900

    CSRF対策

--- www/html/login_process.php
+++ www/html/login_process.php
@@ -17,19 +17,19 @@ $db = get_db_connect();
 
 $user = login_as($db, $name, $password);
 
-if ($user === false) {
-  set_error('ログインに失敗しました。');
-  redirect_to(LOGIN_URL);
-}
-
 if (is_valid_csrf_token($token)) {
-  set_message('ログインしました。');
-  if ($user['type'] === USER_TYPE_ADMIN) {
-    redirect_to(ADMIN_URL);
+  if ($user === false) {
+    set_error('ログインに失敗しました。');
+    redirect_to(LOGIN_URL);
   }
 } else {
   set_error('不正な操作が行われました。');
   redirect_to(LOGIN_URL);
+}
+
+set_message('ログインしました。');
+if ($user['type'] === USER_TYPE_ADMIN) {
+  redirect_to(ADMIN_URL);
 }
 
 redirect_to(HOME_URL);
